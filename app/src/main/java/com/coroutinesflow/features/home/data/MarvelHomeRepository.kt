5ab0373a16a84997d1d65542bfdb7a5e012f160b
AbdElRahman <AbdElRahman.Gamal-AbdElSatar@vodafone.com>
2020-04-07 18:45:38 +0200

    add local DB "room" and handle logic of repo

--- app/src/main/java/com/coroutinesflow/features/home/data/MarvelHomeRepository.kt
+++ app/src/main/java/com/coroutinesflow/features/home/data/MarvelHomeRepository.kt
@@ -9,7 +9,6 @@ import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.flow.onStart
-import kotlinx.coroutines.withContext
 
 class MarvelHomeRepository(
     private val marvelHomeRemoteDataStore: MarvelHomeRemoteDataStore,
@@ -19,24 +18,22 @@ class MarvelHomeRepository(
 
     @ExperimentalCoroutinesApi
     suspend fun getListOfMarvelHeroesCharacters(limit: Int = 10, offset: Int = 0) = flow {
-
-            marvelHomeLocalDataStore.getListOfMarvelHeroesCharacters(limit, offset).collect {
-
-                if (it.isNotEmpty()) {
-                    emit(APIState.DataStat(it))
-                } else {
-                    marvelHomeRemoteDataStore.getListOfMarvelHeroesCharacters(limit, offset)
-                        .collect { states ->
-                            when (states) {
-                                is APIState.LoadingState -> emit(APIState.LoadingState)
-                                is APIState.ErrorState -> emit(APIState.ErrorState(states.exception))
-                                is APIState.DataStat -> {
-                                    emit(APIState.DataStat(states.value.data.results))
-                                    marvelHomeLocalDataStore.updateInsert(states.value.data.results)
-                                }
+        marvelHomeLocalDataStore.getListOfMarvelHeroesCharacters(limit, offset).collect {
+            if (it.isNotEmpty()) {
+                emit(APIState.DataStat(it))
+            } else {
+                marvelHomeRemoteDataStore.getListOfMarvelHeroesCharacters(limit, offset)
+                    .collect { states ->
+                        when (states) {
+                            is APIState.LoadingState -> emit(APIState.LoadingState)
+                            is APIState.ErrorState -> emit(APIState.ErrorState(states.exception))
+                            is APIState.DataStat -> {
+                                emit(APIState.DataStat(states.value.data.results))
+                                marvelHomeLocalDataStore.updateInsert(states.value.data.results)
                             }
                         }
-                }
+                    }
+            }
         }
     }.onStart { emit(APIState.LoadingState) }.flowOn(iODispatcher)
 
